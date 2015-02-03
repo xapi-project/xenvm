17738afaaf0fd97f951cd0ee4e99bf441942f18e
David Scott <dave.scott@citrix.com>
2015-02-03 23:03:16 +0000

    Move the masterJournal and {to,from}LVM queues to shared storage
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- setup.sh
+++ setup.sh
@@ -9,12 +9,12 @@ lvcreate -L 256M djstest -n free
 lvcreate -L 256M djstest -n live
 
 # create journals: one for a slave, one for a master
+lvcreate -L 1M djstest -n masterJournal
 dd if=/dev/zero of=localJournal bs=1M count=1
-dd if=/dev/zero of=masterJournal bs=1M count=1
 
 # create the to/FromLVM rings for one host
-dd if=/dev/zero of=toLVM bs=1M count=1
-dd if=/dev/zero of=fromLVM bs=1M count=1
+lvcreate -L 1M djstest -n toLVM
+lvcreate -L 1M djstest -n fromLVM
 
 echo Run 'sudo ./local-allocator.native' and type 'djstest-live' to request an allocation
 echo Run 'sudo ./remote-allocator.native' to see the LVM updates being picked up
