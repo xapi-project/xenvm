1c066e7d87ebe121561c69c2a2d7a19d4f80c7cd
David Scott <dave.scott@citrix.com>
2015-02-09 11:12:20 +0000

    cleanup: can now handle broken volume groups
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,9 @@
+dmsetup remove djstest-live
+dmsetup remove djstest-free
+dmsetup remove djstest-masterJournal
+dmsetup remove djstest-toLVM
+dmsetup remove djstest-fromLVM
+dd if=/dev/zero of=/dev/loop0 bs=1M count=128
 lvremove -f djstest/free
 lvremove -f djstest/live
 vgremove -f djstest
