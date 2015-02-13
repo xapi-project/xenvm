b19b6b6f053320c735a6eca1ba6fcf95d5525935
David Scott <dave.scott@citrix.com>
2015-02-13 20:41:53 +0000

    test: switch from lvm to xenvm
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,5 @@ dmsetup remove djstest-masterJournal
 dmsetup remove djstest-toLVM
 dmsetup remove djstest-fromLVM
 dd if=/dev/zero of=/dev/loop0 bs=1M count=128
-lvremove -f djstest/free
-lvremove -f djstest/live
-vgremove -f djstest
+killall xenvmd.native
 losetup -d /dev/loop0
--- setup.sh
+++ setup.sh
@@ -2,22 +2,21 @@
 rm -f bigdisk
 dd if=/dev/zero of=bigdisk bs=1 seek=1G count=0
 losetup /dev/loop0 bigdisk
-pvcreate --metadatasize=10M /dev/loop0
-vgcreate djstest /dev/loop0
-# Volume djstest-free will contain the free blocks
-lvcreate -L 4M djstest -n free
-# Volume djstest-live is the guest LV
-lvcreate -L 4M djstest -n live
+./xenvmd.native&
+sleep 1s # bind to socket
+./xenvm.native format /dev/loop0 --vg djstest --host 127.0.0.1
+./xenvm.native open /dev/loop0 --host 127.0.0.1
+LVS="free live masterJournal toLVM fromLVM"
+for i in ${LVS}; do
+  echo Creating $i
+  ./xenvm.native create --lv $i --host 127.0.0.1
+done
+for i in ${LVS}; do
+  echo Activating $i
+  ./xenvm.native activate --lv $i --host 127.0.0.1
+done
 
-# create journals: one for a slave, one for a master
-lvcreate -L 1M djstest -n masterJournal
 dd if=/dev/zero of=localJournal bs=1M count=1
-# Clear the local free block list
-rm -f djstest-free
-
-# create the to/FromLVM rings for one host
-lvcreate -L 1M djstest -n toLVM
-lvcreate -L 1M djstest -n fromLVM
 
 echo Run 'sudo ./local-allocator.native' and type 'djstest-live' to request an allocation
 echo Run 'sudo ./remote-allocator.native' to see the LVM updates being picked up
