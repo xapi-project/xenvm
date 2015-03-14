afb05cd736cdca68655eb136faa878f78425d08e
David Scott <dave.scott@citrix.com>
2015-03-14 15:47:18 +0000

    test: fix bisect, upload to coveralls only works from travis
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- test.sh
+++ test.sh
@@ -4,17 +4,19 @@ eval `opam config env`
 make
 
 # Making a 1G disk
-rm -f bigdisk xenvm*.out
+rm -f bigdisk _build/xenvm*.out
 dd if=/dev/zero of=bigdisk bs=1 seek=16G count=0
 
-BISECT_FILE=xenvm.coverage ./xenvm.native format bigdisk --vg djstest
-BISECT_FILE=xenvmd.coverage ./xenvmd.native --config ./test.xenvmd.conf &
+BISECT_FILE=_build/xenvm.coverage ./xenvm.native format bigdisk --vg djstest
+BISECT_FILE=_build/xenvmd.coverage ./xenvmd.native --config ./test.xenvmd.conf &
 
-export BISECT_FILE=xenvm.coverage
+export BISECT_FILE=_build/xenvm.coverage
 
 ./xenvm.native create --lv live
 ./xenvm.native benchmark
 
 ./xenvm.native shutdown
-bisect-report xenvm*.out -summary-only -html /vagrant/report/
-`opam config var bin`/ocveralls xenvm*.out --send
+wait $(pidof xenvmd.native)
+(cd _build; bisect-report xenvm*.out -summary-only -html /vagrant/report/)
+echo Sending to coveralls
+`opam config var bin`/ocveralls --prefix _build _build/xenvm*.out --send || echo "Failed to upload to coveralls (this only works from travis)"
