e878de909f7cc1171364b968a4ffbc4a186e8fcc
David Scott <dave.scott@eu.citrix.com>
2015-03-19 21:01:15 +0000

    xenvm: resize locally if device is active AND we have an allocator
    Otherwise we continue to use the rpc interface. This will be needed
    to handle resizing of offline devices.
    
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- xenvm/lvresize.ml
+++ xenvm/lvresize.ml
@@ -6,7 +6,6 @@ open Lwt
 let lvresize copts (vg_name,lv_opt) real_size percent_size =
   let lv_name = match lv_opt with | Some l -> l | None -> failwith "Need an LV name" in
   let open Xenvm_common in
-  let local_allocator_path = match copts.Xenvm_common.local_allocator_path with | Some p -> p | None -> failwith "Need a local allocator path" in
   let size = match parse_size real_size percent_size with
   | `IncreaseBy x -> `IncreaseBy x
   | `Absolute x -> `Absolute x
@@ -16,24 +15,33 @@ let lvresize copts (vg_name,lv_opt) real_size percent_size =
     set_uri copts info;
     Client.get_lv ~name:lv_name >>= fun (vg, lv) ->
     if vg.Lvm.Vg.name <> vg_name then failwith "Invalid VG name";
-    let name = Mapper.name_of vg lv in
-    Lwt.catch
-      (fun () ->
-        let s = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
-        Lwt_unix.connect s (Unix.ADDR_UNIX local_allocator_path)
-        >>= fun () ->
-        let oc = Lwt_io.of_fd ~mode:Lwt_io.output s in
-        let r = { ResizeRequest.local_dm_name = name; action = size } in
-        Lwt_io.write_line oc (Sexplib.Sexp.to_string (ResizeRequest.sexp_of_t r))
-        >>= fun () ->
-        Lwt_io.close oc
-      ) (fun _ ->
-        (* XXX: we assume the local allocator is missing because this provisioning
-           is disabled. *)
-        match size with
-        | `Absolute size -> Client.resize lv_name size
-        | `IncreaseBy delta -> Client.resize lv_name Int64.(add delta (mul (mul 512L vg.Lvm.Vg.extent_size) (Lvm.Lv.size_in_extents lv)))
-      )
+
+    let resize_remotely () = match size with
+    | `Absolute size -> Client.resize lv_name size
+    | `IncreaseBy delta -> Client.resize lv_name Int64.(add delta (mul (mul 512L vg.Lvm.Vg.extent_size) (Lvm.Lv.size_in_extents lv))) in
+
+    let resize_locally allocator =
+      let name = Mapper.name_of vg lv in
+      let s = Lwt_unix.socket Lwt_unix.PF_UNIX Lwt_unix.SOCK_STREAM 0 in
+      Lwt_unix.connect s (Unix.ADDR_UNIX allocator)
+      >>= fun () ->
+      let oc = Lwt_io.of_fd ~mode:Lwt_io.output s in
+      let r = { ResizeRequest.local_dm_name = name; action = size } in
+      Lwt_io.write_line oc (Sexplib.Sexp.to_string (ResizeRequest.sexp_of_t r))
+      >>= fun () ->
+      Lwt_io.close oc in
+
+    match copts.Xenvm_common.local_allocator_path with
+    | None ->
+      (* safe to allocate remotely *)
+      resize_remotely ()
+    | Some allocator ->
+      let name = Mapper.name_of vg lv in
+      let all = Devmapper.ls () in
+      (* An active device with local allocator running must be resized locally *)
+      if List.mem name all
+      then resize_locally allocator
+      else resize_remotely ()
   )
 
 let lvresize_cmd =
