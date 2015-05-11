3032e192a712d889b54a86d3f4f255c3514083f6
David Scott <dave.scott@eu.citrix.com>
2015-05-11 18:14:24 +0100

    test: move more of the common code to the common module
    Signed-off-by: David Scott <dave.scott@eu.citrix.com>

--- test/common.ml
+++ test/common.ml
@@ -12,6 +12,29 @@
  * GNU Lesser General Public License for more details.
  *)
 
+open OUnit
+open Lvm
+open Vg
+open Lwt
+
+module Log = struct
+  let debug fmt = Printf.ksprintf (fun s -> print_endline s) fmt
+  let info  fmt = Printf.ksprintf (fun s -> print_endline s) fmt
+  let error fmt = Printf.ksprintf (fun s -> print_endline s) fmt
+end
+
+module Time = struct
+  type 'a io = 'a Lwt.t
+  let sleep = Lwt_unix.sleep
+end
+
+module Vg_IO = Vg.Make(Log)(Block)(Time)(Clock)
+
+let (>>*=) m f = match Lvm.Vg.error_to_msg m with
+  | `Error (`Msg e) -> fail (Failure e)
+  | `Ok x -> f x
+let (>>|=) m f = m >>= fun x -> x >>*= f
+
 (* Utility functions common to all scripts.
    Perhaps these should be moved into the Xcp_service library? *)
 
@@ -191,3 +214,42 @@ let tib = Int64.(gib * kib)
 
 module Client = Xenvm_client.Client
 
+let with_temp_file fn =
+  let filename = "/tmp/vg" in
+  let f = Unix.openfile filename [Unix.O_CREAT; Unix.O_RDWR; Unix.O_TRUNC] 0o644 in
+  (* approximately 10000 4MiB extents for volumes, 100MiB for metadata and
+     overhead *)
+  let _ = Unix.lseek f (1024*1024*4*10100 - 1) Unix.SEEK_SET in
+  ignore(Unix.write f "\000" 0 1);
+  Unix.close f;
+  let result = fn filename in
+  Unix.unlink filename;
+  result
+  (* NB we leak the file on error, but this is quite useful *)
+
+let with_loop_device path f =
+  ignore_string (run "losetup" [ "-f"; path ]);
+  (* /dev/loop0: [fd00]:1973802 (/tmp/SR.createc04251volume) *)
+  let line = run "losetup" [ "-j"; path ] in
+  try
+    let i = String.index line ' ' in
+    let device = String.sub line 0 (i - 1) in
+    finally
+      (fun () -> f device)
+      (fun () -> run "losetup" [ "-d"; device ] |> ignore_string)
+  with e ->
+    error "Failed to parse output of losetup -j: [%s]" line;
+    ignore_string (run "losetup" [ "-d"; path ]);
+    failwith (Printf.sprintf "Failed to parse output of losetup -j: [%s]" line)
+
+let with_block filename f =
+  let open Lwt in
+  Block.connect (Printf.sprintf "buffered:%s" filename)
+  >>= function
+  | `Error x ->
+    fail (Failure (Printf.sprintf "Unable to read %s" filename))
+  | `Ok x ->
+    f x (* no point catching errors here *)
+
+let xenvm = run "./xenvm.native"
+let xenvmd = run "./xenvmd.native"
--- test/test.ml
+++ test/test.ml
@@ -18,73 +18,12 @@ open Vg
 open Common
 open Lwt
 
-module Log = struct
-  let debug fmt = Printf.ksprintf (fun s -> print_endline s) fmt
-  let info  fmt = Printf.ksprintf (fun s -> print_endline s) fmt
-  let error fmt = Printf.ksprintf (fun s -> print_endline s) fmt
-end
-
-module Time = struct
-  type 'a io = 'a Lwt.t
-  let sleep = Lwt_unix.sleep
-end
-
-module Vg_IO = Vg.Make(Log)(Block)(Time)(Clock)
-
-let (>>*=) m f = match Lvm.Vg.error_to_msg m with
-  | `Error (`Msg e) -> fail (Failure e)
-  | `Ok x -> f x
-let (>>|=) m f = m >>= fun x -> x >>*= f
-
-let with_dummy fn =
-  let filename = "/tmp/vg" in
-  let f = Unix.openfile filename [Unix.O_CREAT; Unix.O_RDWR; Unix.O_TRUNC] 0o644 in
-  (* approximately 10000 4MiB extents for volumes, 100MiB for metadata and
-     overhead *)
-  let _ = Unix.lseek f (1024*1024*4*10100 - 1) Unix.SEEK_SET in
-  ignore(Unix.write f "\000" 0 1);
-  Unix.close f;
-  let result = fn filename in
-  Unix.unlink filename;
-  result
-  (* NB we leak the file on error, but this is quite useful *)
-
-let with_loop_device path f =
-  ignore_string (run "losetup" [ "-f"; path ]);
-  (* /dev/loop0: [fd00]:1973802 (/tmp/SR.createc04251volume) *)
-  let line = run "losetup" [ "-j"; path ] in
-  try
-    let i = String.index line ' ' in
-    let device = String.sub line 0 (i - 1) in
-    finally
-      (fun () -> f device)
-      (fun () -> run "losetup" [ "-d"; device ] |> ignore_string)
-  with e ->
-    error "Failed to parse output of losetup -j: [%s]" line;
-    ignore_string (run "losetup" [ "-d"; path ]);
-    failwith (Printf.sprintf "Failed to parse output of losetup -j: [%s]" line)
-
-let with_block filename f =
-  let open Lwt in
-  Block.connect (Printf.sprintf "buffered:%s" filename)
-  >>= function
-  | `Error x ->
-    fail (Failure (Printf.sprintf "Unable to read %s" filename))
-  | `Ok x ->
-    f x (* no point catching errors here *)
-
-let pv = Result.get_ok (Pv.Name.of_string "pv0")
-let small = Int64.(mul (mul 1024L 1024L) 4L)
-let small_extents = 1L
-
-let xenvm = run "./xenvm.native"
-let xenvmd = run "./xenvmd.native"
 let vg = "myvg"
 
 let vgcreate =
-  "vgcreate <vg name> <device>" >::
+  "vgcreate <vg name> <device>: check that we can create a volume group and re-read the metadata." >::
   fun () ->
-  with_dummy (fun filename ->
+  with_temp_file (fun filename ->
     xenvm [ "vgcreate"; vg; filename ] |> ignore_string;
     let t =
       with_block filename
@@ -103,13 +42,19 @@ let no_xenvmd_suite = "Commands which should work without xenvmd" >::: [
 
 let assert_lv_exists name =
   let t =
-    Client.get_lv "test"
-    >>= fun (lv, vg) ->
-    return () in
+    Lwt.catch
+      (fun () ->
+        Client.get_lv "test"
+        >>= fun (_lv, _vg) ->
+        return ()
+      ) (fun e ->
+        Printf.fprintf stderr "xenvmd claims the LV %s doesn't exist: %s\n%!" name (Printexc.to_string e);
+        failwith (Printf.sprintf "LV %s doesn't exist" name)
+      ) in
   Lwt_main.run t
 
 let lvcreate_L =
-  "lvcreate -n <name> -L <mib> <vg>" >::
+  "lvcreate -n <name> -L <mib> <vg>: check that we can create an LV with a size in MiB" >::
   fun () ->
   xenvm [ "lvcreate"; "-n"; "test"; "-L"; "4"; vg ] |> ignore_string;
   assert_lv_exists "test";
@@ -121,7 +66,7 @@ let xenvmd_suite = "Commands which require xenvmd" >::: [
 
 let _ =
   run_test_tt_main no_xenvmd_suite |> ignore;
-  with_dummy (fun filename' ->
+  with_temp_file (fun filename' ->
     with_loop_device filename' (fun loop ->
       xenvm [ "vgcreate"; vg; loop ] |> ignore_string;
       mkdir_rec "/etc/xenvm.d" 0o0644;
