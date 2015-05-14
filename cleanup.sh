ce6753a340f6a07af6a454328ff992fbb3157952
David Scott <dave.scott@citrix.com>
2015-05-14 09:34:39 +0000

    Fix the setup.sh and cleanup.sh scripts, for easier testing
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,13 @@
-./xenvm.native shutdown
+set -ex
+
+if [ "$EUID" -ne "0" ]; then
+  echo "Please run me as uid 0. I need to create a loop device and device mapper devices"
+  exit 1
+fi
+
+./xenvm.native lvchange -an /dev/djstest/live || true
+#./xenvm.native shutdown /dev/djstest
 killall xenvmd.native
-LVS="live"
-for i in ${LVS}; do
-  echo Removing $i
-  rm -f ./djstest-$i
-done
 dmsetup remove_all
 dd if=/dev/zero of=/dev/loop0 bs=1M count=128
 losetup -d /dev/loop0
--- setup.sh
+++ setup.sh
@@ -1,20 +1,39 @@
 set -e
-# Making a 1G disk on /dev/loop0
-rm -f bigdisk
-dd if=/dev/zero of=bigdisk bs=1 seek=16G count=0
+set -x
+
+# There is an important bugfix in the master branch. If you see
+# ECONNREFUSED you might need this:
+# opam pin add conduit git://github.com/mirage/ocaml-conduit -y
+# If you are using a XenServer build environment then the patch is
+# already included in the RPM.
+
+if [ "$EUID" -ne "0" ]; then
+  echo "Please run me as uid 0. I need to create a loop device and device mapper devices"
+  exit 1
+fi
+
+# Making a 1G disk
+rm -f bigdisk _build/xenvm*.out
+dd if=/dev/zero of=bigdisk bs=1 seek=256G count=0
+echo Using /dev/loop0
 losetup /dev/loop0 bigdisk
-BISECT_FILE=xenvm.coverage ./xenvm.native format /dev/loop0 --vg djstest
-BISECT_FILE=xenvmd.coverage ./xenvmd.native &
+cat test.xenvmd.conf.in | sed -r "s|@BIGDISK@|/dev/loop0|g" > test.xenvmd.conf
+mkdir -p /etc/xenvm.d
+BISECT_FILE=_build/xenvm.coverage ./xenvm.native format /dev/loop0 --vg djstest
+BISECT_FILE=_build/xenvmd.coverage ./xenvmd.native --config ./test.xenvmd.conf --daemon
 
-export BISECT_FILE=xenvm.coverage
+export BISECT_FILE=_build/xenvm.coverage
 
-./xenvm.native create --lv live
-./xenvm.native activate --lv live `pwd`/djstest-live /dev/loop0
+./xenvm.native set-vg-info --pvpath /dev/loop0 -S /tmp/xenvmd djstest --local-allocator-path /tmp/xenvm-local-allocator --uri file://local/services/xenvmd/djstest
 
-./xenvm.native benchmark
+./xenvm.native lvcreate -n live -L 4 djstest
+./xenvm.native lvchange -ay /dev/djstest/live
 
-./xenvm.native host-create host1
-./xenvm.native host-connect host1
+#./xenvm.native benchmark
 
-echo Run 'sudo ./local_allocator.native' and type 'djstest-live' to request an allocation
+./xenvm.native host-create /dev/djstest host1
+#./xenvm.native host-connect /dev/djstest host1
+
+#echo Run 'sudo ./xenvm.native host-connect /dev/djstest host1' to connect to the local allocator'
+#echo Run 'sudo ./local_allocator.native' and type 'djstest-live' to request an allocation
 echo Run './cleanup.sh' to remove all volumes and devices
