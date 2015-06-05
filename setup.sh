3eedb931ecde12e36fd32f4ca28b44516465bb41
David Scott <dave.scott@citrix.com>
2015-06-05 12:36:15 +0000

    setup.sh: run `vgchange -ay` and `vchange -an` to check they succeed
    Signed-off-by: David Scott <dave.scott@citrix.com>

--- setup.sh
+++ setup.sh
@@ -36,6 +36,10 @@ mkdir -p /tmp/xenvm.d
 ./xenvm.native set-vg-info --pvpath $LOOP -S /tmp/xenvmd djstest --local-allocator-path /tmp/host1-socket --uri file://local/services/xenvmd/djstest --configdir /tmp/xenvm.d $MOCK_ARG
 
 ./xenvm.native lvcreate -n live -L 4 djstest --configdir /tmp/xenvm.d $MOCK_ARG
+
+./xenvm.native vgchange /dev/djstest -ay --configdir /tmp/xenvm.d $MOCK_ARG
+./xenvm.native vgchange /dev/djstest -an --configdir /tmp/xenvm.d $MOCK_ARG
+
 ./xenvm.native lvchange -ay /dev/djstest/live --configdir /tmp/xenvm.d $MOCK_ARG
 
 ./xenvm.native lvdisplay /dev/djstest --configdir /tmp/xenvm.d $MOCK_ARG
