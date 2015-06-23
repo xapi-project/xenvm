ca5944f3bddff420d88772e44851a665c6de1cac
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-06-23 11:24:28 +0100

    Make sure we truncate the pidfile when opening
    This avoids writing incorrect pids
    
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- idl/pidfile.ml
+++ idl/pidfile.ml
@@ -3,7 +3,7 @@
 let write_pid pidfile =
   let txt = string_of_int (Unix.getpid ()) in
   try
-    let fd = Unix.openfile pidfile [ Unix.O_WRONLY; Unix.O_CREAT ] 0o0644 in
+    let fd = Unix.openfile pidfile [ Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC ] 0o0644 in
     Unix.lockf fd Unix.F_TLOCK (String.length txt);
     let (_: int) = Unix.write fd txt 0 (String.length txt) in
     ()
