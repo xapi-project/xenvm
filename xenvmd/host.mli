39989f3dfc731cc79e3cc67af5bbf2637635c7ea
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-10-02 14:48:20 +0100

    Constrain host signature
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- xenvmd/host.mli
+++ xenvmd/host.mli
@@ -7,18 +7,11 @@ type connected_host = {
 }
 val get_connected_hosts : unit -> (string * connected_host) list
 val get_connected_host : string -> connected_host option
-val toLVM : string -> string
-val fromLVM : string -> string
-val freeLVM : string -> string
 val create : string -> unit Lwt.t
-val sexp_of_exn : exn -> Sexplib.Sexp.t
 val connect : string -> unit Lwt.t
-val flush_m : Lwt_mutex.t
-val flush_already_locked : string -> unit Lwt.t
 val disconnect : cooperative:bool -> string -> unit Lwt.t
 val destroy : string -> unit Lwt.t
 val all : unit -> Xenvm_interface.host list Lwt.t
 val reconnect_all : unit -> unit Lwt.t
-val flush_one : string -> unit Lwt.t
 val flush_all : unit -> unit Lwt.t
 val shutdown : unit -> unit Lwt.t
